37fd4e05853cc4db362dec2fd2017b4b6ff573c6
Danylo Sahaidak <danylos2001@gmail.com>
2020-06-03 15:43:40 +0300

    add pages and anchors

--- web_page.py
+++ web_page.py
@@ -1,31 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    """
-    First main page.
-    """
-    return render_template("index.html")
+    return redirect("/home")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
-@app.route("/alien", methods=["POST"])
+@app.route("/alien")
 def alien():
-    """
-    Page with dictionary.
-    """
-    
-    return render_template("created_dict.html")
+    return render_template("alien.html")
 
-@app.route("/human", methods=["POST"])
+@app.route("/human")
 def human():
-    """
-    Page with dictionary.
-    """
-    
-    return render_template("created_dict.html")
+    return render_template("human.html")
 
 
 if __name__ == '__main__':
